57084fa35f3e4f49cfe6087e7caf9d4ab3b9c494
Angel Francisco <franciscoangel807@gmail.com>
2024-05-23 14:41:27 -0500

    I think I fixed it

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
 print("Hello world")
 
-<<<<<<< HEAD
-print("Como te llamas?!! ")
-=======
-input("Como te llamas? ")
->>>>>>> 386706d6bfeb207434bcb213738686d901dee2f4
+input("Como te llamas?!! ")
 print("angel")
